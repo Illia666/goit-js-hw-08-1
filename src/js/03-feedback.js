7006197de6479d0b6bbde925ca219c0e45de7b67
Illia666 <108007460+Illia666@users.noreply.github.com>
2022-11-23 00:11:19 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 
+import storage from './refuge';
+import throttle from 'lodash.throttle';
+
+const FORM_VALUES_KEY = 'feedback-form-state';
+
+const saveFeedback = throttle(event => {
+  // search parent form element
+  if (event.currentTarget) {
+    let from = event.currentTarget;
+    while (from.nodeName !== 'FORM') {
+      from = from.parentNode;
+    }
+
+    // collect object from form fields
+    const data = new FormData(from);
+
+    const formValues = {};
+    data.forEach((value, key) => {
+      formValues[key] = value;
+    });
+    // save to local storage
+    storage.saveToStorage(FORM_VALUES_KEY, formValues);
+  }
+}, 500);
+
+// fill form fields from local storage
+const values = storage.loadFromStorage(FORM_VALUES_KEY);
+const form = document.querySelector('.feedback-form');
+
+for (let input of form.elements) {
+  if (input.name) {
+    if (values && values[input.name]) {
+      input.value = values[input.name];
+    }
+    input.addEventListener('input', saveFeedback);
+  }
+}
+
+// reset form
+const handleSubmit = event => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.clear();
+};
+form.addEventListener('submit', handleSubmit);
